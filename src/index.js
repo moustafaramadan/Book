eb6e4e9dae6d765830d44e8929e05c24ee888da1
moustafaramadan <95917190+moustafaramadan@users.noreply.github.com>
2023-04-03 02:33:08 +0200

    setup gh-page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render((
+   <BrowserRouter basename={process.env.PUBLIC_URL}>
+     <App />
+   </BrowserRouter>
+),
   document.getElementById("root")
 );
